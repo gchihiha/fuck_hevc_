c20301c841ab7e5df9958fbe99be1eb0411ff496
gchihiha <1318405440@qq.com>
2020-09-25 18:04:10 +0800

    还是改成inline函数好了

--- fuck_hevc/bit_stream.hpp
+++ fuck_hevc/bit_stream.hpp
@@ -5,15 +5,38 @@
 # include <cstdlib>
 
 # define bs_name
+# define Enable_REG 
+
+# define DECL_bitstream_as_param \
+size_t bs_name##_buf_, \
+size_t bs_name##_buf2_, \
+size_t bs_name##_bsPos_
+
+# define USE_bitstream_to_func \
+bs_name##_buf_, \
+bs_name##_buf2_, \
+bs_name##_bsPos_
+
+# define DECL_bitstream_infunc \
+Enable_REG size_t bs_name ## _buf = bs_name##_buf_; \
+Enable_REG size_t bs_name ## _buf2 = bs_name##_buf2_; \
+Enable_REG size_t bs_name ## _bsPos = bs_name##_bsPos_; \
 
 # define DECL_bitstream \
-register size_t bs_name##_buf;\
-register size_t bs_name##_buf2;\
-register size_t bs_name##_bsPos;\
-register size_t* bs_name##_ptr; \
-size_t* bs_name##_data; \
-size_t bs_name##_data_size;
+static size_t*  bs_name ## _data; \
+static size_t   bs_name ## _data_size; \
+static size_t   bs_name ## _buf;\
+static size_t   bs_name ## _buf2;\
+static size_t   bs_name ## _bsPos;\
+\
+/*temp for use*/\
+static size_t   bs_name ## _pos; \
+static size_t   bs_name ## _offset;\
+static size_t   bs_name ## _zCount;\
 
+constexpr size_t cpu_bit_count() {
+    return sizeof(size_t) * 8;
+}
 constexpr size_t cpu_word_rShift_count() {
     switch (sizeof(size_t)) {
     case 1:
@@ -37,28 +60,44 @@ constexpr size_t rBitmask(size_t bit_count) {
 # define word_offset (bs_name##_bsPos & rBitmask(cpu_word_rShift_count()))
 # define byte_offset (bs_name##_bsPos & 0xFF)
 
+# define curr_byte_ptr ((uint8_t*)bs_name##_data)
+# define curr_byte ( curr_byte_ptr[byte_pos] )
 
-# if defined(_WIN64)
-    # define REG_A rax
-    # define REG_B rbx
-# elif defined(_WIN32)
-    # define REG_A eax
-    # define REG_B ebx
+# define flush_buf_fast \
+bs_name##_buf = bs_name##_data[word_pos]; bs_name##_buf=reverse(bs_name##_buf);
 
-# endif
+# define flush_buf \
+bs_name##_pos = word_pos; bs_name##_offset = word_offset; \
+bs_name##_buf = bs_name##_data[bs_name##_pos]; bs_name##_buf=reverse(bs_name##_buf); bs_name##_buf<<bs_name##_offset;\
+bs_name##_buf2=bs_name##_data[bs_name##_offset+1]; bs_name##_buf |= bs_name##_buf2>>(cpu_bit_count()-bs_name##_offset);
+
 
 # if defined(_MSC_VER)
+
+    # if defined(_WIN64)
+        # define REG_A rax
+        # define REG_B rbx
+        # define reverse(n__) _byteswap_uint64(n__)
+        # define rotr(v__, n__) _lrotr(v__, n__)
+
+    # elif defined(_WIN32)
+        # define REG_A eax
+        # define REG_B ebx
+        # define reverse(n__) _byteswap_ulong(n__)
+        # define rotr(v__, n__) _rotr64(v__, n__)
+        static_assert(sizeof(unsigned long) == 4, "sizeof(unsigned long) != 4");
+    # endif
     # define rzc(ret__, n__) _asm{ bsf REG_A,n__; mov ret__,REG_A; }
     # define lzc(ret__, n__) _asm{ bsr REG_B,n__; mov ret__,REG_A; }
 
 # elif defined(__GNUC__)
+
     # define rzc(ret__, n__) 
     # define lzc(ret__, n__) 
+    # define reverse(n__) 
 
 # endif
-// 待办
-# define flush_buf bs_name##_buf = bs_name##_data[word_offset];
-# define flush_buf2 bs_name##_buf2 = bs_name##_data[word_offset];
+
 
 
 
@@ -80,40 +119,30 @@ more_data_in_byte_stream（）指定如下：
 next_bits（n）提供位流中的下一位用于比较目的，而无需移动位流指针。
 提供以n为参数的比特流的下n个位。 当在附件B中指定的字节流格式中使用并且字节流中剩余的位数少于n个时，next_bits（n）返回值0。
 */
-# define bs_next(ret__, n__)    
+# define bs_next(n__) (bs_name##_buf & rBitmask(n__))
 
 /*
 read_bits（n）从比特流中读取下一个n位，并将比特流指针前移n位。 当n等于0时，指定read_bits（n）以返回等于0的值并且不超前位流指针。
 */
-# define bs_read(ret__, n__) 
+# define bs_read(ret__, n__) ret__ = bs_next( n__); bs_name##_buf>>=n__; bs_name##_bsPos+=n__
 
 /*具有任何位串模式（8位）的字节。 该描述符的解析过程由函数read_bits（8）的返回值指定。*/
-# define bs_b8(ret__) 
+# define bs_b8(ret__) bs_read(ret__, 8)
 
 /*使用n位的无符号整数。
 此描述符的解析过程由函数read_bits（n）的返回值指定，该函数的值解释为无符号整数的二进制表示形式，其中最高有效位先写入。*/
-# define bs_u(ret__, n__)
+# define bs_u(ret__, n__) bs_read(ret__, n__)
+
+/*
+使用n位的有符号整数。
+该描述符的解析过程由函数read_bits（n）的返回值指定，该函数的值解释为二进制补码整数表示形式，其中最高有效位先写入。
+*/
+# define bs_i(ret__, n__) 
 
 /*
 固定模式的位字符串，使用从左到右写入的n位（从左到右）。 该描述符的解析过程由函数read_bits（n）的返回值指定。
 */
-# define bs_f(ret__, n__)
-
-/*
-使用n位的有符号整数。 
-该描述符的解析过程由函数read_bits（n）的返回值指定，该函数的值解释为二进制补码整数表示形式，其中最高有效位先写入。
-*/
-# define bs_i(ret__, n__)
-
-/*
-se（v）：带符号的整数0阶Exp-Golomb编码语法元素，其左位在前。 第9.2节规定了该描述符的解析过程。
-*/
-# define bs_se(ret__, v__)
-
-/*
-ae（v）：上下文自适应算术熵编码的语法元素。 9.3节规定了该描述符的解析过程。
-*/
-# define bs_ae(ret__, v__) 
+# define bs_f(ret__, n__) bs_read(ret__, n__)
 
 /*
 st（v）：空终止字符串，编码为ISO / IEC 10646中指定的通用编码字符集（UCS）传输格式8（UTF-8）字符。
@@ -123,13 +152,30 @@ st（v）开始于 比特流中的一个字节对齐位置，
 从当前位置开始，一直延续到但不包括下一个等于0x00的字节对齐字节，并将比特流指针前进 （stringLength + 1）* 8位位置，
 其中stringLength等于返回的字节数。
 */
-# define bs_st(ret__, v__)
+# define bs_st(str__) str__ = curr_byte_ptr; bs_name##_bsPos += str__.size()*8
+
 
 /*
-ue（v）：无符号整数0阶Exp-Golomb编码语法元素，其左位在前。 
+ue（v）：无符号整数0阶Exp-Golomb编码语法元素，其左位在前。
 第9.2节规定了该描述符的解析过程。
 */
-# define bs_ue(ret__, v__)
+# define bs_ue(ret__)\
+bs_name##_zCount = bs_name##_bsPos; \
+while(true) {\
+size_t count = 0; \
+rzc(count,bs_name##_buf); \
+bs_name##_bsPos+=count; \
+flush_buf; \
+if(count < cpu_bit_count()){ break; } \
+} \
+bs_name##_zCount = bs_name##_bsPos-bs_name##_lz; \
+ret__ = ((1<<bs_name##_zCount)-1) + bs_read(bs_name##_zCount)
+
+
+/*
+se（v）：带符号的整数0阶Exp-Golomb编码语法元素，其左位在前。 第9.2节规定了该描述符的解析过程。
+*/
+# define bs_se(ret__) bs_ue(ret__); (*(size_t*)&ret__) = rotr((size_t)ret__,1);
 
 
 
@@ -138,6 +184,13 @@ ue（v）：无符号整数0阶Exp-Golomb编码语法元素，其左位在前。
 
 
 
+
+
+
+/*
+ae（v）：上下文自适应算术熵编码的语法元素。 9.3节规定了该描述符的解析过程。 待办 没看懂9.3
+*/
+# define bs_ae(ret__, v__) 
 
 /*
 more_data_in_payload（）指定如下：
--- fuck_hevc/syntax.hpp
+++ fuck_hevc/syntax.hpp
@@ -1,6 +1,89 @@
-#pragma once
-# include <cstdint>
+# pragma once
+# include "bit_stream.hpp"
+
+# include <iostream>
+# include <vector>
+
+# define dbg_show(...)      std::cout<< __FILE__ <<" "<<__LINE__<<" "<<__VA_ARGS__<<std::endl;
+# define wrn_show(...)      std::cout<< __FILE__ <<" "<<__LINE__<<" "<<__VA_ARGS__<<std::endl;
+# define err_show(...)      std::cout<< __FILE__ <<" "<<__LINE__<<" "<<__VA_ARGS__<<std::endl;
+# define detail_show(...)   std::cout<< __FILE__ <<" "<<__LINE__<<" "<<__VA_ARGS__<<std::endl;
+
+struct H265 {
+
+# define undef bs_name
+# define undef Enable_REG
+
+# define bs_name h264bs
+# define Enable_REG register
+
+    DECL_bitstream;
+
+    struct Nal_unit {
+        static constexpr size_t nalu_reserve = 8192;
+
+        uint8_t forbidden_zero_bit;
+        uint8_t nal_unit_type;
+        uint8_t nuh_layer_id;
+        uint8_t nuh_temporal_id_plus1;
+        std::vector<uint8_t> data;
+
+        Nal_unit()
+            :forbidden_zero_bit(0)
+            , nal_unit_type(0)
+            , nuh_layer_id(0)
+            , nuh_temporal_id_plus1(0)
+            , data(0)
+        {
+            data.reserve(nalu_reserve);
+        }
+        void read(size_t NumBytesInNalUnit) {
+            
+            flush_buf_fast;
+            bs_f(forbidden_zero_bit, 1);
+            bs_u(nal_unit_type, 6);
+            bs_u(nuh_layer_id, 6);
+            bs_u(nuh_temporal_id_plus1, 3);
+
+            data.clear();
+            uint8_t curr_data;
+
+            for (size_t i = 2; i < NumBytesInNalUnit; i++) {
+                bs_read(curr_data, 8);
+                data.push_back(curr_data);
+
+                if ((i + 2) < NumBytesInNalUnit && bs_next(24) == 0x000003) {
+                    bs_read(curr_data, 8);
+                    data.push_back(curr_data);
+                    i += 2;
+                    uint8_t emulation_prevention_three_byte;
+                    bs_f(emulation_prevention_three_byte, 8);   /* equal to 0x03 */
+                    if (emulation_prevention_three_byte != 0x3) {
+                        err_show("emulation_prevention_three_byte");
+                    }
+                }
+                bs_read(curr_data, 8);
+                data.push_back(curr_data);
+            }
+
+        }
+
+    };
+
+    static void nal_unit(std::vector<uint32_t>&data_) {
 
 
 
-    
+    }
+
+
+
+
+};
+
+
+
+
+
+
+
